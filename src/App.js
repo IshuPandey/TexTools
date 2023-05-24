7c54426085ca48357db48ffb82f8b3e343441df1
IshuPandey <ishupandey39@gmail.com>
2023-05-24 13:51:51 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,74 +1,58 @@
+import { useState } from "react";
+import "./App.css";
 
-import { useState } from 'react';
-import './App.css';
-/* import About from './components/About'; */
-import Alert from './components/Alert';
-import Navbar from './components/Navbar';
-import Textform from './components/Textform';
-/* import {
-  BrowserRouter as Router,
-  Routes,
-  Route
- 
-} from "react-router-dom"; */
-
+import Alert from "./components/Alert";
+import Navbar from "./components/Navbar";
+import Textform from "./components/Textform";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./components/About";
 
 function App() {
-  const [mode,setmode]=useState('light');
-  const [alert,setAlert]=useState(null);
+  const [mode, setmode] = useState("light");
+  const [alert, setAlert] = useState(null);
 
-  const showAlert = (message,type)=>{
+  const showAlert = (message, type) => {
     setAlert({
-       msg:message,
-        type:type
-            })
-            setTimeout(()=>{
-              setAlert(null);
-            
-            },2000) 
-
-  }
-  const toggleMode=()=>
-  {
-    if(mode==='light'){
-      setmode('dark');
-      document.body.style.backgroundColor='black';
-      showAlert("Dark mode enabled","success");
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+  const toggleMode = () => {
+    if (mode === "light") {
+      setmode("dark");
+      document.body.style.backgroundColor = "black";
+      showAlert("Dark mode enabled", "success");
+    } else {
+      setmode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode enabled", "success");
     }
-    else{
-      setmode('light');
-      document.body.style.backgroundColor='white';
-      showAlert("Light mode enabled","success");
-    }
-  }
+  };
   return (
-<>
+    <>
+      <Router>
+        <Navbar title="TexTools" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
 
-
-{/* <Router> */}
-<Navbar title="TexTools" mode={mode} toggleMode={toggleMode} />
-<Alert alert={alert}/>
-
-<div className="container my-3" >
-{/* <Routes> */}
-{/* <Route path="/" exact element={ */}
-<Textform mode={mode} showAlert={showAlert} />
-
-          
-         
-{/* <Route path="/About" element={ <About mode={mode} />} /> */}
-           
-          
-
-         
-{/* </Routes> */}
-     
-</div>  
-
-
-{/* </Router> */}
-
-</>
+        <div className="container my-3">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={<Textform mode={mode} showAlert={showAlert} />}
+            />
+            <Route
+              exact
+              path="/about"
+              element={<About mode={mode} showAlert={showAlert} />}
+            />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
